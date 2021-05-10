ee692b62899717f057b3fbd52c2bd94d5599ce37
SebastianAp27 <61700621+SebastianAp27@users.noreply.github.com>
2021-05-10 14:32:06 +0300

    2 more tests

--- GirdTheGrid.Test/UnitTest.cs
+++ GirdTheGrid.Test/UnitTest.cs
@@ -30,6 +30,28 @@ namespace GirdTheGrid.Test
             
             cut.Find("p1").MarkupMatches(@"<p1>Insert a region name and see how many power stations there are and what is the total energy output</p1>");
         }
+        [Fact]
+        public void CanSeeStatistics2IfAuthenticatedAndAuthorized()
+        {
+            using var ctx = new TestContext();
+            var authContext = ctx.AddTestAuthorization();
+            authContext.SetAuthorized("TEST USER", AuthorizationState.Authorized);
 
+            var cut = ctx.RenderComponent<Statistics>();
+
+            cut.Find("p2").MarkupMatches(@"<p2>Insert 2 numbers and see which power stations have their energy output within that interval:</p2>");
+        }
+        [Fact]
+        public void CanSeeStatistics3IfAuthenticatedAndAuthorized()
+        {
+            using var ctx = new TestContext();
+            var authContext = ctx.AddTestAuthorization();
+            authContext.SetAuthorized("TEST USER", AuthorizationState.Authorized);
+
+            var cut = ctx.RenderComponent<Statistics>();
+
+            cut.Find("p3").MarkupMatches(@"<p3>Insert a station Type and see all the powerStations that have that type </p3>");
+        }
+       
     }
 }
