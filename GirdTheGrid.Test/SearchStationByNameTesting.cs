53690267fa733c26dbb21f5ec8405d92ee29c7ae
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2021-05-24 00:53:12 +0300

    + controller testing

---
+++ GirdTheGrid.Test/SearchStationByNameTesting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GirdTheGrid2.Shared
+{
+    public class GivenANameStation
+    {
+        private readonly SearchPowerStation _searchPowerStation;
+        private readonly Filters _sut;
+
+        public GivenANameStation()
+        {
+            _searchPowerStation = new SearchPowerStation("name");
+            _sut = new Filters(_searchPowerStation);
+        }
+
+
+        public class WhenGettingAStation : GivenANameStation
+        {
+
+            [Fact]
+            public ShouldGetValidStation()
+            {
+                var expectedValue = true;
+                var actualValue = _sut.Post(_searchPowerStation);
+
+                Assert.Equal(expectedValue, actualValue);
+            }
+        }
+
+    }
+}
