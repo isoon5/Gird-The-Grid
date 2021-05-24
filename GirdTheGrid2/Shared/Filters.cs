523035379956dd263da9c86e685b19d90bb9c2ca
SebastianAp27 <61700621+SebastianAp27@users.noreply.github.com>
2021-05-24 12:15:24 +0300

    5/24/2021

---
+++ GirdTheGrid2/Shared/Filters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirdTheGrid2.Shared
+{
+    public class Filters
+    {
+        public string StationName { get; set; }
+         
+        public string StationType { get; set; }
+        
+        public float EnergyOutput { get; set; }
+        
+        public string StationRegion { get; set; }
+    }
+}
